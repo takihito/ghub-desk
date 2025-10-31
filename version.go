fdc408ff38bc58883d71b9a6972151e108f4abc8
akihito <takeda-akihito@kayac.com>
2025-10-31 12:48:36 +0900

    v02

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package main
 
 var (
 	// version is set during build via ldflags
-	Version = "v0.9.0"
+	Version = "v0.2.0"
 	// commit is set during build via ldflags. see Makefile.
 	Commit = "none"
 	// date is set during build via ldflags. see Makefile.
